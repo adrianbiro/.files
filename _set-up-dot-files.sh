ef8479d7de493cf9b3ad990715b0b739f15f02dd
Adrian Biro <biroadrian@protonmail.com>
2024-03-02 16:26:53 +0100

    small fixes

---
+++ _set-up-dot-files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+declare -a files
+files=(
+    ".bashrc"
+    #".vimrc"  # cf. in .bashrc VIMINIT
+    ".bash_profile"
+    ".curlrc"
+    ".emacs"
+    ".gdbinit"
+    ".gitconfig"
+    ".inputrc"
+    ".nanorc"
+    ".psqlrc"
+    ".screenrc"
+    ".tmux.conf"
+    )
+if [[ "${PWD##*/}" != "dot_files" ]]; then
+    echo "Run from dot_files directory."
+    exit 1
+fi
+for i in "${files[@]}"; do
+    ln -sf "${PWD}/${i}" "${HOME}/${i}"
+done
